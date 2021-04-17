7ff0c7784bd2a05561d6dd7db0e7b3c8cff3508a
Ryan Ma <ryanma@Ryvn-Laptop.lan>
2021-04-17 01:20:41 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {});
+
+app.listen(3000, function () {
+  console.log("server is running on port 3000");
+});
+
+let count = document.getElementById("count");
+document.getElementsByClassName("seat").addEventLister("click", function () {});
